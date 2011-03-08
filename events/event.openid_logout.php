e06eb408adc500a64a6b1df7b9d3dd066ed61807
bauhouse <bauhouse@gmail.com>
2011-03-08 09:17:41 -0800

    Create OpenID Logout event

---
+++ events/event.openid_logout.php
@@ -0,0 +1,35 @@
+<?php
+
+	require_once TOOLKIT . '/class.event.php';
+
+	class EventOpenID_Logout extends Event{
+		
+		public static function about(){
+			return array(
+					 'name' => 'OpenID Logout',
+					 'author' => array(
+							'name' => 'Stephen Bau',
+							'email' => 'stephen@domain7.com'),
+					 'version' => '1.0',
+					 'release-date' => '2011-03-08');	
+		}
+
+		public static function allowEditorToParse(){
+			return false;
+		}
+
+		public static function documentation(){
+			return '
+				<p>Log out of an OpenID session.</p>
+			';
+		}
+
+		public function load(){
+			if(isset($_GET['openid-action']['logout'])) return $this->__trigger();
+		}
+
+		protected function __trigger() {
+			$cookie = new Cookie('openid', TWO_WEEKS, __SYM_COOKIE_PATH__);
+			$cookie->expire();
+		}
+	}
