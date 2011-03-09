808f3ac637d06e921a05c28cabf2e1177bee1bb4
Stephen Bau <bauhouse@gmail.com>
2011-03-08 20:29:38 -0800

    Add OpenID parameters to the parameter pool

--- extension.driver.php
+++ extension.driver.php
@@ -21,6 +21,11 @@
 					'page' => '/frontend/',
 					'delegate' => 'openidAuthComplete',
 					'callback' => 'authenticationComplete'
+				),
+				array(
+					'page' => '/frontend/',
+					'delegate' => 'FrontendParamsResolve',
+					'callback' => 'addParamsToPage'
 				)
 			);
 		}
@@ -31,5 +36,21 @@
 			$cookie = new Cookie('openid', TWO_WEEKS, __SYM_COOKIE_PATH__);
 			$cookie->set('identifier', $openid_data->identifier);
 			$cookie->set('sreg-data',  $openid_data->sreg_data);
+		}
+
+		/**
+		 * Add OpenID parameters to parameter pool
+		 */
+		public function addParamsToPage(array $context=array()){
+
+			$cookie = new Cookie('openid', TWO_WEEKS, __SYM_COOKIE_PATH__);
+			$openid_cookie_data = $cookie->get('sreg-data');
+			
+			if($openid_cookie_data) {
+				$context['params']['openid-cookie-email'] = $openid_cookie_data[email];
+				$context['params']['openid-cookie-first'] = $openid_cookie_data[first];
+				$context['params']['openid-cookie-last'] = $openid_cookie_data[last];
+			}
+
 		}
 	}
